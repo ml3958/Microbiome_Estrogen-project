b4c7c6a71a82b71e6de90f7c5e0f9411f04cdbf7
Menghan Liu <lium14@nyumc.org>
2015-09-28 14:21:57 -0400

    Update Estrogen.py

--- Estrogen.py
+++ Estrogen.py
@@ -24,4 +24,16 @@ alpha_rarefaction.py -i otu_table_rdp_nochimera_m0001.biom  -o diversity/a_raref
 # output <alpha_mapping_09242015.txt>
 add_alpha_to_mapping_file.py -i adiv_pd.txt -m ~/Documents/Research/Rotation/Blaser\ lab/Estrogen/Qiime/Barcode_Metadata.txt -o alpha_mapping_09242015.txt
 
-
+## 9.28
+#compute new alpha diversity with new metadata file
+alpha_rarefaction.py -i otu_table_rdp_nochimera_m0001.biom  -o diversity/alpha_div/a_rarefaction2_09282015/ -t rep_set.tre  -m metadata_mapping_files/Mappingfiles_Sample_EStro_Barcode_09252015.txt
+#Compute beta diviersity
+# 1. non phylogenetic
+beta_diversity.py -i otu_table_rdp_nochimera_m0001.biom -m euclidean -o diversity/beta_div
+# 2. phylogenetic
+beta_diversity.py -i otu_table_rdp_nochimera_m0001.biom -m weighted_unifrac -o diversity/beta_div/weighted_unifrac_betadiv.txt -t rep_set.tre
+# 3. Multiple File (batch from multiple rarefraction.py) Beta Diversity (phylogenetic)
+multiple_rarefactions.py -i otu_table_rdp_nochimera_m0001.biom -m 10 -x 140 -s 10 -n 2 -o rarefied_otu_tables/
+beta_diversity.py -i  rarefied_otu_tables/ -m weighted_unifrac -o -o diversity/beta_div/ -t rep_set.tre
+#compute beta diversity through plot
+beta_diversity_through_plots.py -i otu_table_rdp_nochimera_m0001.biom -o diversity/beta_div/bdiv_even100/ -t rep_set.tre -m metadata_mapping_files/Mappingfiles_Sample_EStro_Barcode_09252015.txt -e 100
